06c9ec3e58143e1c871463decd85751c2efbb08b
M. Palladino <Michael21115@yahoo.com>
2016-04-08 00:14:57 -0400

    Testing Fog of War, room generation

---
+++ Assets/Scripts/fogOfWarDetect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fogOfWarDetect : MonoBehaviour {
+
+	public GameObject roomLight;
+
+
+	void OnTriggerEnter (Collider collider){
+		Debug.Log("Something Enters; is it a player?");
+		if (collider.tag == "Player"){
+			Debug.Log("Yes, it's a player");
+			roomLight.SetActive(true);
+		}
+	}
+
+	void OnTriggerExit(Collider collider){
+		if (collider.tag == "Player"){
+			roomLight.SetActive(false);
+		} 
+	}
+}
