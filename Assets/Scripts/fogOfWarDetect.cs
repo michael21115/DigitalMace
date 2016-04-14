6e777285b40cba576041c1b0ae34a4e0e8a65e47
M. Palladino <Michael21115@yahoo.com>
2016-04-14 14:36:06 -0400

    5 x 5 grid

--- Assets/Scripts/fogOfWarDetect.cs
+++ Assets/Scripts/fogOfWarDetect.cs
@@ -5,17 +5,50 @@ public class fogOfWarDetect : MonoBehaviour {
 
 	public GameObject roomLight;
 
+//	void Start(){
+//		// Randomizes lights on/off at the start of the game
+//		float lightCheck = Random.value;
+//		if (lightCheck <= .9f){
+//			roomLight.SetActive(false);
+//		}
+//		else {
+//			roomLight.SetActive(true);
+//		}
+//	}
 
+	// Randomly turn lights off
+	void Update () {
+		float flickerCheck = Random.value;
+		if (flickerCheck <= .005f){
+			if (roomLight.activeInHierarchy){			
+				roomLight.SetActive(false);
+			}
+// 			TURN ON LIGHTS RANDOMLY
+//			else if (roomLight.activeInHierarchy == false){
+//				roomLight.SetActive(true);
+//			} 
+			// ... or else nothing happens
+			else {
+				return;
+			}
+		}
+	}
+
+	// FOG OF WAR ACTIVATION
+	// very simple player detection
 	void OnTriggerEnter (Collider collider){
-		Debug.Log("Something Enters; is it a player?");
+		// if an object with the player tag enters a room
 		if (collider.tag == "Player"){
 			Debug.Log("Yes, it's a player");
+			// turn on the lights
 			roomLight.SetActive(true);
 		}
 	}
 
+	// if an object with the player tag leaves the room
 	void OnTriggerExit(Collider collider){
 		if (collider.tag == "Player"){
+			// turn off the lights
 			roomLight.SetActive(false);
 		} 
 	}
