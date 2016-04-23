10b844043ea5a269537c8b2045b1e711fcbb26e8
M. Palladino <Michael21115@yahoo.com>
2016-04-23 13:29:17 -0400

    Beginning merge of elements

---
+++ Assets/Scripts/EntryRoomClear.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntryRoomClear : MonoBehaviour {
+
+	float timer;
+	
+	// Update is called once per frame
+	void Update () {
+	transform.position += new Vector3 (0f, 0f, -0.1f);
+
+	timer += Time.deltaTime;
+		if (timer > 1f) {
+			Destroy(gameObject);
+		}
+
+	}
+
+	void OnTriggerEnter (Collider collider){
+		if (collider.gameObject.tag == "Wall"){
+			Debug.Log("I hit a wall, I want to get rid of it");
+			Destroy(collider);
+		}
+	}
+}
