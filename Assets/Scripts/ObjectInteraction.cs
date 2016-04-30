aba9d66527332f133a38db0052a31266c38985af
michael21115 <michael21115@yahoo.com>
2016-04-30 18:32:27 -0400

    Revert "I merged shit and cried at home cause I didnt want to cry in front of Calvin"

---
+++ Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectInteraction : MonoBehaviour {
+
+    [SerializeField] public Transform[] hands;
+    public float throwForce;
+    public bool throwItem, keyItem;
+    public string throwItemName, keyItemName;
+    public float objectMass;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (hands[0].childCount == 0)
+        {
+            throwItem = false;
+            throwItemName = null;
+        }
+        if (hands[1].childCount == 0)
+        {
+            keyItem = false;
+            keyItemName = null;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Rigidbody otherRB = other.gameObject.GetComponent<Rigidbody>();
+
+        if (other.collider.tag == "Projectile")
+        {
+            otherRB.mass = 1;
+            GetComponent<Rigidbody>().AddForce((Vector3.up * 500f));
+
+            if (keyItem)
+            {
+                Rigidbody keyItemRB = hands[1].FindChild(keyItemName).GetComponent<Rigidbody>();
+
+                keyItemRB.constraints = RigidbodyConstraints.None;
+                keyItemRB.constraints = RigidbodyConstraints.FreezeRotation;
+                keyItemRB.detectCollisions = true;
+                keyItemRB.useGravity = true;
+                hands[1].DetachChildren();
+            }
+        }
+
+        if (other.collider.tag == "Grabby Thing" && !throwItem)
+        {
+            objectMass = other.gameObject.GetComponent<ThrowableObjects>().mass;
+            throwItemName = other.gameObject.name;
+
+                otherRB.constraints = RigidbodyConstraints.FreezeAll;
+                otherRB.mass = 0;
+                other.transform.position = hands[0].position;
+                other.transform.SetParent(hands[0]);
+            otherRB.mass = 0f;
+                otherRB.useGravity = false;
+                throwItem = true;
+        }
+        else if (other.collider.tag == "Key Item" && !keyItem)
+        {
+            keyItemName = other.gameObject.name;
+
+            otherRB.constraints = RigidbodyConstraints.FreezeAll;
+            other.transform.position = hands[1].position;
+            other.transform.SetParent(hands[1]);
+            otherRB.detectCollisions = false;
+            otherRB.useGravity = false;
+            keyItem = true;
+        }
+    }
+}
